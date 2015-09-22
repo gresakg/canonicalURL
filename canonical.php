f357cbbdb4ddab97866b7d2c856087d36681c0a9
polhec <tine.dolzan@gmail.com>
2015-09-22 08:19:25 +0200

    first commit

---
+++ canonical.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package Canonical URL
+ * @version 0.1 
+ */
+/*
+Plugin Name: Canonical URL
+Plugin URI: http:// 
+Description: Canonical URL
+Author: Tine Dolžan
+Version: 0.1 
+Author URI: http://
+*/
+
+add_action( 'wp_head', 'canonical_URL' );
+
+function canonical_URL()
+{
+
+
+}
